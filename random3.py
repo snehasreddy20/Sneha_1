367c82adde60b8aca34b6455f426c32fc1e83feb
snehasreddy20 <140402795+snehasreddy20@users.noreply.github.com>
2023-07-25 15:33:07 +0530

    random 3 pgm

---
+++ random3.py
@@ -0,0 +1,15 @@
+def duplicate(l):
+    result = []
+    for item in l:
+        if item not in result:
+            result.append(item)
+    return result
+
+
+my_lst = [1, 2, 3, 4, 2, 3, 5, 6, 4]
+result = duplicate(my_lst)
+print("result_lst",result)
+print("oringinal lst",my_lst)
+x=my_lst[0:4]
+print(x)
+print(my_lst[0])
