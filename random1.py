6bc81177480bc61c8af0f7ea88b5bdadd6924c98
snehasreddy20 <140402795+snehasreddy20@users.noreply.github.com>
2023-07-25 15:09:35 +0530

    Add files via upload

---
+++ random1.py
@@ -0,0 +1,12 @@
+my_lst = [1, 2, 3, 4, 2, 3, 5, 6, 4]
+result = duplicate(my_lst)
+print("result_lst",result)
+print("oringinal lst",my_lst)
+x=my_lst[0:4]
+print(x)
+print(my_lst[0])
+
+l1=[1,2,2,3,4,5]
+l2 = {"my":23,"likes":45,44:22}
+print(f"l2 ha {l2[44]} subcreibers {l2[my]} {l2[44]}")
+
