a9b16c052f8555b3fb7942b9ac11bfdb58221c88
snehasreddy20 <140402795+snehasreddy20@users.noreply.github.com>
2023-07-26 21:09:15 +0530

    Update lambda.py

--- random1.py
+++ random1.py
@@ -1,8 +1,28 @@
-my_lst = [1, 2, 3, 4, 2, 3, 5, 6, 4]
-result = duplicate(my_lst)
-print("result_lst",result)
-print("oringinal lst",my_lst)
-x=my_lst[0:4]
-print(x)
-print(my_lst[0])
 
+#  1: Map with Lambda
+
+
+num = [1, 2, 3, 4, 5]
+squared_num = list(map(lambda x: x**2, num))
+print(squared_num)
+
+### 2: Zip
+
+names = ['Alice', 'Bob', 'Charlie']
+ages = [25, 30, 35]
+A = list(zip(names, ages))
+print(A)
+
+
+# 3: Filter
+num = [1, 2, 3, 4, 5]
+even = list(filter(lambda x: x % 2 == 0, num))
+print(even)
+
+
+# Example 4: Reduce
+
+from functools import reduce
+num = [1, 2, 3, 4, 5]
+sum_of_numbers = reduce(lambda x, y: x + y, num)
+print(sum_of_numbers) 
